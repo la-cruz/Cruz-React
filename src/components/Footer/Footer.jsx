a7c08eb80229d8b7644397ed2b522463022e1f51
Mickael Alves <0mickael.alves0@gmail.com>
2020-08-29 16:27:18 +0200

    Fix warning

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
         <footer>
             <div className="cruz-footer">
                 <section className="links-footer">
-                    <li><a href="https://www.linkedin.com/company/association-la-cruz/" target="_blank" rel="noopener" aria-label="Link to our linkedin page"><i className="fab fa-linkedin"></i></a></li>
-                    <li><a href="https://github.com/LA-CRUZ" target="_blank" rel="noopener" aria-label="Link to our github page"><i className="fab fa-github"></i></a></li>
-                    <li><a href="mailto:contact@lacruz.org" target="_blank" rel="noopener" aria-label="Link to our mail"><i className="fas fa-envelope"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/association-la-cruz/" target="_blank" rel="noopener noreferrer" aria-label="Link to our linkedin page"><i className="fab fa-linkedin"></i></a></li>
+                    <li><a href="https://github.com/LA-CRUZ" target="_blank" rel="noopener noreferrer" aria-label="Link to our github page"><i className="fab fa-github"></i></a></li>
+                    <li><a href="mailto:contact@lacruz.org" target="_blank" rel="noopener noreferrer" aria-label="Link to our mail"><i className="fas fa-envelope"></i></a></li>
                 </section>
                 <section className="copyright">
                     <p><Link to="/mentions">Mentions légales </Link> - © La Cruz<span><img src={require('../../assets/images/logo_cruz.png')} alt="logo cruz" /></span></p>
