f2d6f3ec4b41cdb71fc80be53c8c850a736284ea
Etienne I <etienne.idoux@etu.univ-lyon1.fr>
2020-08-29 16:40:49 +0200

    legal done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from "react-i18next";
 import ScrollToTop from './components/Basic/ScrollToTop'
 import Error404 from './components/Basic/Error404'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
+import Legal from './components/Legal/Legal'
 
 import {
   BrowserRouter as Router,
@@ -16,11 +17,36 @@ function App() {
   const { i18n } = useTranslation()
   i18n.changeLanguage("fr")
 
+  useEffect( () => {
+    const logo = document.querySelector(".cruz-logo > img");
+      if(window.innerWidth > 1200) {
+        logo.style.width = "5%";
+        logo.style.left = "5%";
+        logo.style.top = "5%";
+        logo.style.opacity = "1";
+      } else if(window.innerWidth < 500) {
+          var header = document.createElement("div");
+          header.className = "header-responsive";
+          document.body.appendChild(header);
+
+          window.onscroll = function() {
+              if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                  header.style.opacity = "1";
+              } else {
+                  header.style.opacity = "0";
+              }
+          }
+      }
+  }, []);
+
   return (
     <Router>
       <ScrollToTop />
       <Header />
       <Switch>
+        <Route path="/legal">
+          <Legal />
+        </Route>
         <Route exact path="/">
           <Home />
         </Route>
