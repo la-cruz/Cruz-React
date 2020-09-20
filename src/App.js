f22187aaf49077b04789853e63923934ca412328
Etienne I <etienne.idoux@etu.univ-lyon1.fr>
2020-09-20 14:41:07 +0200

    projects get all folder & components init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { useTranslation } from "react-i18next";
+
 import ScrollToTop from './components/Basic/ScrollToTop'
 import Error404 from './components/Basic/Error404'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
-
+import Project from './components/Project/Project'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
+// Projects
+import projects from "./lib/projects";
+
+
+
+
+
 function App() {
   const { i18n } = useTranslation()
   i18n.changeLanguage("fr")
+  console.log(projects);
 
   return (
     <Router>
@@ -24,6 +33,15 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
+        {
+          projects.map( (project) => {
+            return (
+              <Route path={"/" + project.name} key={project.name}>
+                <Project data={project} />
+              </Route>
+            )
+          })
+        }
         <Route>
           <Error404 />
         </Route>
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Project( {data} ) {
+    return (
+        <div className="project">
+        </div>
+    );
+};
+
+export default Project;
