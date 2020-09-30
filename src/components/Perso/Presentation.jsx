398940567e6013ba8969a19779a0b2d3c4400a44
Etienne I <etienne.idoux@etu.univ-lyon1.fr>
2020-09-30 18:09:05 +0200

    add cv for etienne & cv button in perso page is functionnal

--- src/components/Perso/Presentation.jsx
+++ src/components/Perso/Presentation.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { Trans } from 'react-i18next'
+import i18n from 'i18next';
 
-function Perso({ data }) {
+function Perso({data}) {
+  const language = i18n.language;
+  const fileName = language === "fr" ? data.cv_fr : data.cv_en;
+  var resume = null;
+  
+  try {
+    resume = fileName === "" ? null : require("../../assets/cvs/" + fileName);
+  } catch (error) {};
+
   return (
     <section className="perso-presentation">
       <img src={require(`../../assets/images/team/big.png`)} />
@@ -13,6 +22,12 @@ function Perso({ data }) {
             data.socials.map(link => (
               <li key={link.icon}><a href={link.link}><i className={link.icon}></i></a></li>
             ))
+          }
+          {
+            resume !== null &&
+            <li>
+              <a href={resume} download={fileName}><i class="fas fa-file-download"></i></a>
+            </li>
           }
         </ul>
       </div>
