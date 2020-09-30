6069f33d56f9888847e7161169dffd31d854e0b3
Etienne I <etienne.idoux@etu.univ-lyon1.fr>
2020-09-30 17:30:30 +0200

    smooth scroll button on project page

--- src/components/Project/Features.jsx
+++ src/components/Project/Features.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import { forwardRef } from 'react';
 import { Trans } from 'react-i18next';
 import SlickLink from '../Basic/SlickLink'
 
-function Features({data}) {
-    return (
-        <div className="features">
-            <h2><Trans>label.project.features</Trans></h2>
-            <SlickLink data={data} type="feature"/>
-        </div>
-    );
-}
+const Features = forwardRef( ({data}, ref) => (
+    <div className="features" ref={ref}>
+        <h2><Trans>label.project.features</Trans></h2>
+        <SlickLink data={data} type="feature"/>
+    </div>
+));
 
 export default Features;
