98a1bce72e374653f84b34a43c65482f390e57b8
Etienne I <etienne.idoux@etu.univ-lyon1.fr>
2020-10-03 18:27:47 +0200

    page perso sabard done

--- src/components/Perso/RelatedProject.jsx
+++ src/components/Perso/RelatedProject.jsx
@@ -5,7 +5,12 @@ import { Trans } from 'react-i18next'
 function RelatedProject({ projects }) {
   return (
     <section className="related-project">
-      <h2><Trans>label.perso.projects</Trans></h2>
+      {
+        projects.length === 0 ?
+          <h2><Trans>label.perso.noProjects</Trans></h2>
+        :
+          <h2><Trans>label.perso.projects</Trans></h2>
+      }
       <SlickLink data={projects} type="project-perso"></SlickLink>
     </section>
   )
