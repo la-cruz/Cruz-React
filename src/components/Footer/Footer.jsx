516d3373a6ab2d6960a507b8cbdd4b70ac0900b7
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-10-11 17:47:31 +0200

    Adding Audart + Alves Bio and Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,11 +16,11 @@ function Footer() {
     return (
         <footer>
             <div className="cruz-footer">
-                <section className="links-footer">
+                <ul className="links-footer">
                     <li><a href="https://www.linkedin.com/company/association-la-cruz/" target="_blank" rel="noopener noreferrer" aria-label="Link to our linkedin page"><i className="fab fa-linkedin"></i></a></li>
                     <li><a href="https://github.com/LA-CRUZ" target="_blank" rel="noopener noreferrer" aria-label="Link to our github page"><i className="fab fa-github"></i></a></li>
                     <li><a href="mailto:contact@lacruz.org" target="_blank" rel="noopener noreferrer" aria-label="Link to our mail"><i className="fas fa-envelope"></i></a></li>
-                </section>
+                </ul>
                 <section className="copyright">
                     <p><Link to="/legal"><Trans>label.footer.legal</Trans> </Link> - © La Cruz<span><img src={require('../../assets/images/logo_cruz.png')} alt="logo cruz" /></span></p>
                 </section>
