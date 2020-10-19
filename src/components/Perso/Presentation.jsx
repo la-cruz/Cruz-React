30ab153155fc0ad791f1b0b0394336f83b0d9ec6
Mickael Alves <0mickael.alves0@gmail.com>
2020-10-19 12:23:57 +0200

    Fix home perso

--- src/components/Perso/Presentation.jsx
+++ src/components/Perso/Presentation.jsx
@@ -8,8 +8,7 @@ function Perso({ data, scrollToRef }) {
     var resume;
 
     try {
-        resume = fileName === "" ? null : require("../../assets/cvs/" + fileName);
-        console.log(resume);
+        resume = fileName === "" ? "" : require('../../assets/cvs/' + fileName);
     } catch (error) {
         resume = null;
     };
@@ -31,8 +30,8 @@ function Perso({ data, scrollToRef }) {
                     }
                 </ul>
                 {
-                    resume !== null &&
-                    <a data-aos="fade-left" className="hover-shadow cv" data-aos-delay="1000" href={resume} download={fileName}><i className="fas fa-file-download"></i><Trans>label.perso.cv</Trans></a>
+                    resume !== "" &&
+                    <a data-aos="fade-left" className="hover-shadow cv" data-aos-delay="1000" ><i className="fas fa-file-download"></i><Trans>label.perso.cv</Trans></a>
                 }
             </div>
             <picture className="scroll" onClick={scrollToRef}>
